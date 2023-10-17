820e2f04d113365ae884355e65e7ae8e908643aa
Zoe <zoe.boehme@student.unisg.ch>
2023-10-17 20:16:13 +0200

    Merged with Patrick's version

--- Fabrik.java
+++ Fabrik.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
  * aufnehmen und Informationen zu Bestellungen ausgeben. 
  *
  * @author Gruppe 1
- * @version 1.2
+ * @version 1.3
  */
 public class Fabrik
 {
@@ -27,22 +27,35 @@ public class Fabrik
         // wird die Nummer um 1 erhöht
         bestellungsNr = 0;
     }
-
-      /**
+    
+    /**
      * Methode mit der eine neue Bestellung aufgegeben werden kann.
+     * Falls die Bestellung zulässig ist, wird sie aufgenommen und User 
+     * werden dementsprechend informiert.
+     * Negative Zahlen und eine Bestellung mit 0 Stühlen und Sofas 
+     * sind nicht zulässig. Falls ein User eine unzulässige Bestellung
+     * aufgeben will, wird informiert, dass dies nicht geht.
+     * Die Methode gibt keinen Wert zurück.
      * Die Bestellnummer wird mit jeder neuen Bestellung erhöht,
-     * sodass keine Bestellung die gleiche Nummer hat
+     * sodass keine Bestellung die gleiche Nummer hat.
      * @param stuehle Anzahl der bestellten Stühle.
      * @param sofas Anzahl der bestellten Sofas
      */
     
-    public void bestellungAufgeben(int stuehle, int sofas)
-    {
-    
-        bestellungen.add(new Bestellung(bestellungsNr, stuehle, sofas));
-        // Wert der Bestellnummer wird erhöht
-        bestellungsNr++;
+    public void bestellungAufgeben(int stuehle, int sofas) {
+        if(stuehle < 0 || sofas < 0){ 
+            System.out.println("Negative Anzahl von Stühlen oder Sofas ist unzulässig. Auftrag nicht eingereicht.");
+        } else if (stuehle == 0 && sofas == 0){  
+            System.out.println("Auftrag für 0 Stühle und 0 Sofas kann nicht bearbeitet werden.");
+        } else {
+            bestellungen.add(new Bestellung(bestellungsNr, stuehle, sofas));
+            bestellungsNr++;
+            System.out.println("Auftrag erhalten! Anzahl Stühle: " + stuehle + ", Anzahl Sofas: " + sofas +".");
+        }
+
     }
+
+
     
     /**
      * Bestellungen können auf Basis der Bestellnummer gesucht werden
