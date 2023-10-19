573d186ffe183b60373ee1c49d7fae5e37e4c149
Lindafuchs24 <131275689+Lindafuchs24@users.noreply.github.com>
2023-10-19 22:42:58 +0200

    Update Bestellung.java

--- Bestellung.java
+++ Bestellung.java
@@ -10,95 +10,106 @@
 public class Bestellung
 {
     {
-    //Die privaten Attribute werden definiert
-    //für die bestellbestätigung wit ein boolean Attribut erstellt, welches überprüft, ob eine Bestellung gemacht wurde
+    //Die privaten Variablen werden definiert
+    //für die bestellbestätigung wit eine Variable des typs boolean erstellt, welches überprüft, ob eine Bestellung gemacht wurde
     private boolean bestellbestaetigung;
-    //für die beschaffungszeit wird ein Integer erstellt, welcher die Lieferdauer misst
+    //für die beschaffungszeit wird eine Integer Variable erstellt, welcher die Lieferdauer misst
     private int beschaffungszeit;
-    //für die bestellnummer wird ein Integer erstellt, welcher einer getätigten Bestellung eine Nummer zuweist
+    //für die bestellnummer wird eine Integer Variable, welcher einer getätigten Bestellung eine Nummer zuweist
     private int bestellnummer;
-    //der Integer anzahlStuehle misst, wie viele Stühle bestellt wurden
+    //die Integer Variable anzahlStuehle misst, wie viele Stühle bestellt wurden
     private int anzahlStuehle;
-    //der Integer anzahlSofas misst, wie viele Sofas bestellt wurden
+    //die Integer Variable anzahlSofas misst, wie viele Sofas bestellt wurden
     private int anzahlSofas;
     // Die ArrayList<Produkt> beinhaltet alle Produkte, die bestellt wurden
     private ArrayList<Produkt> bestellteProdukte;
     }
 
     /**
-     *Der Konstruktor wird erstellt und die Variablen initialisiert.
+     *Der Konstruktor wird erstellt.
      * 
+     * Folgende Variablen werden initialisiert:
      * @param bestellnummer für die Nummer, welcher einer Bestellung zugewiesen wird
      * @param anzahlStuehle für die Bestellte Anzahl von Stühlen
      * @param anzahlSofas für die Bestellte Anzahl von Sofas
-     * @return        die Summe aus x und y
      */
     public int Bestellung(int bestellnummer, int anzahlStuehle, int anzahlSofas)
     {
-        int i;
-        this.anzahlStuehle = anzahlStuehle;
-        this.anzahlSofas = anzahlSofas;
-        bestellteProdukte = new ArrayList();
-        bestellBestaetigung = false;
-        beschaffungsZeit = -1;    
-        this.bestellungsNr = bestellungsNr;
-        
-        for(i=0;i<=anzahlStuehle;i++)
-        {
-            stuhlHinzufuegen();
-        }
-        
-        for(i=0;i<=anzahlSofas;i++)
-        {
-            sofaHinzufuegen();
-        }   
-    }
+      int i;
+      //initialisierung der oben genannten Variablen
+      this.bestellungsNr = bestellungsNr;
+      this.anzahlStuehle = anzahlStuehle;
+      this.anzahlSofas = anzahlSofas;
 
+      //Liste für alle künftigen Bestellungen wird erstellt
+      bestellteProdukte = new ArrayList();
+      //wenn noch keine Bestellung getätigt wurde, beginnt die boolean Variable bei "false"
+      bestellBestaetigung = false;
+      //die Beschaffungszeit beginnt bei 0, wenn noch keine Bestellungen geetätigt wurden
+      beschaffungsZeit = 0;    
+
+      //es weden Variablen initialisiert, mit welchen Stühle und Sofas der Bestellungsliste hinzugefügt werden können
+      for(i=0;i<=anzahlStuehle;i++)
+      {
+         stuhlHinzufuegen();
+      }
+      for(i=0;i<=anzahlSofas;i++)
+      {
+         sofaHinzufuegen();
+      }   
+    }
+    //es werden Methoden erstellt, um neue Bestellunge  für Stühle und Sofas aufzugeben - genauer wird der Liste der Bestellungen ein neuer Stuhl resp. ein neues Sofa hinzugefügt 
     private void stuhlHinzufuegen()
     {
-         Stuhl stuhl = new Stuhl();
-         bestellteProdukte.add(stuhl);       
+      Stuhl stuhl = new Stuhl();
+      bestellteProdukte.add(stuhl);       
     }  
-    
     private void sofaHinzufuegen()
     {
-        Sofa sofa = new Sofa();
-        bestellteProdukte.add(sofa);         
+     Sofa sofa = new Sofa();
+     bestellteProdukte.add(sofa);         
     }  
-    
+
+    //es wird eine Methode erstellt, damit man eine Bestellung machen kann
     public void bestellungBestaetigen()
     {
-        bestellBestaetigung = true;
+     bestellBestaetigung = true;
     } 
-    
+
+    //es wird eine Methode erstellt, um die Bestellbestätigung aufzurufen
     public boolean gibBestellBestaetigung()
     {
-        return bestellBestaetigung;
+     return bestellBestaetigung;
     }  
-    
+
+    //es wird eine Methode erstellt, um die Beschaffungszeit der Bestellung zu definieren
     public void setzteBeschaffungsZeit(int beschaffungszeit)
     {
-        this.beschaffungsZeit = beschaffungsZeit;
+     this.beschaffungsZeit = beschaffungsZeit;
     }  
-    
+
+    //es wiid eine Methode erstellt, um die Beschaffungszeit der Bestellung anzuzeigen
     public int gibBeschaffungsZeit()
     {
-        return beschaffungsZeit;
+     return beschaffungsZeit;
     }  
-    
+
+    //es wird eine Methode erstellt, um die Bestellungsnummer anzuzeigen
     public int gibBestellungsNr()
     {
         return bestellungsNr;
     }
-    
+
+    //es wird eine Methode erstellt, um die bestellte Zahl der Stühle anzuzeigen
     public int gibAnzahlStuehle()
     {
-        return anzahlStuehle;
+    return anzahlStuehle;
     }    
-    
+
+    //es wird eine Methode erstellt, um die bestellte Zahl der Sofas anzuzeigen
     public int gibAnzahlSofas()
     {
-        return anzahlSofas;
+     return anzahlSofas;
     }   
 
 }
